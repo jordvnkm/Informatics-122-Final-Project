0d7add2c1c560acffece1a7205a4900bc870e7dd
bdwalker93 <bdwalker93@yahoo.com>
2016-03-05 14:05:20 -0800

    JSON Files
    Start to JSON for client-server communication

---
+++ 122GameServer/src/Server/JSONTranslator.java
@@ -0,0 +1,20 @@
+package Server;
+
+import org.json.simple.*;
+public class JSONTranslator 
+{
+	public static String gameStateToJSON()
+	{
+		//contains the entire state
+		JSONObject gameState = new JSONObject();
+
+		//stores the game board along with all of its attributes
+		JSONObject board = new JSONObject();
+		
+	
+	
+	
+
+		
+	}
+}
