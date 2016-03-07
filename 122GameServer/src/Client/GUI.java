d36d789c18dd09058b8f2eb3ecf3f857432fa378
Andy <aschlali@uci.edu>
2016-03-07 13:28:58 -0800

    Basic Main Screen of GUI made

--- 122GameServer/src/Client/GUI.java
+++ 122GameServer/src/Client/GUI.java
@@ -1,5 +1,68 @@
 package Client;
 
-public class GUI {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
+public class GUI extends Application{
+
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("INF 122 Game Client");
+        Scene scene = new Scene(new VBox(), 800, 720);
+        
+        //Menus
+        MenuBar mb = new MenuBar();
+        Menu servermenu = new Menu("Server");
+        MenuItem MIselectserv = new MenuItem("Select Server");
+        MenuItem MIplayerprofile = new MenuItem("Player Profile");
+        MenuItem MIExit = new MenuItem("Exit");
+        servermenu.getItems().addAll(MIselectserv,MIplayerprofile,new SeparatorMenuItem(),MIExit);
+        
+        Menu gamemenu = new Menu("Game");
+        MenuItem MIrequestgame = new MenuItem("Request Game");
+        MenuItem MIquitgame = new MenuItem("Quit Game");
+        gamemenu.getItems().addAll(MIrequestgame,MIquitgame);
+        
+        Menu windowmenu = new Menu("Window");
+        MenuItem MIoptions = new MenuItem("Options");
+        windowmenu.getItems().addAll(MIoptions);
+        
+        Menu helpmenu = new Menu("Help");
+        MenuItem MIabout = new MenuItem("About");
+        helpmenu.getItems().addAll(MIabout);
+        
+        mb.getMenus().addAll(servermenu,gamemenu,windowmenu,helpmenu);
+        
+        //canvas to draw on
+        Canvas gameboard = new Canvas (500,500);
+        GraphicsContext gc = gameboard.getGraphicsContext2D();
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, 500, 500);
+        
+        //text area
+        TextArea TAlog = new TextArea();
+        
+        ((VBox) scene.getRoot()).getChildren().addAll(mb,gameboard,TAlog);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
