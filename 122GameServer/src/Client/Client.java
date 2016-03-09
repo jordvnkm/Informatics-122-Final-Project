87b6d7ce439b6a6fb0018ee4346082205a13d592
jordankm <minatogawaj@yahoo.com>
2016-03-08 16:51:26 -0800

    wrote more functions in client class

--- 122GameServer/src/Client/Client.java
+++ 122GameServer/src/Client/Client.java
@@ -30,15 +30,85 @@ public class Client {
 	
 	
 	
+	/////////////////////////////////////////////////////////////////
+	/////sends a request that states which game the player wants to join
+	public void sendGameRequest(String game, String opponent)
+	{
+		JSONObject obj = new JSONObject();
+		obj.put("type", "gameRequest");
+		obj.put("game", game);
+		obj.put("opponent", opponent);
+		
+		try 
+		{
+			OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+		    out.write(obj.toJSONString());
+		    out.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	//////////////////////////////////////////////////////////
+	/// sends a string representing that the button on the GUI has been pressed
+	public void sendButtonPressed()
+	{
+		JSONObject obj = new JSONObject();
+		obj.put("type", "ButtonPressed");
+
+		try 
+		{
+			OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+		    out.write(obj.toJSONString());
+		    out.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	////////////////////////////////////////////////////////////////
+	///// sends a string through the socket requesting players in the server
+	public void requestPlayerList()
+	{
+		JSONObject obj = new JSONObject();
+		obj.put("type", "requestPlayerList");
+
+		try 
+		{
+			OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+		    out.write(obj.toJSONString());
+		    out.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	
 	
 	////////////////////////////////////////////
 	///// sends a string through the socket requesting gamelist
 	public void requestGameList()
 	{	
+		JSONObject obj = new JSONObject();
+		obj.put("type", "requestGameList");
 		try 
 		{
 			OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
-		    out.write("REQUESTING GAMELIST");
+		    out.write(obj.toJSONString());
 		    out.close();
 		}
 		catch (IOException e)
@@ -54,13 +124,14 @@ public class Client {
 	public void sendSelectPiece(int x, int y)
 	{
 		JSONObject obj = new JSONObject();
+		obj.put("type", "selectPiece");
 		obj.put("xVal", x);
 		obj.put("yVal", y);
 		
 		try 
 		{
 			OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
-		    out.write(obj.toString());
+		    out.write(obj.toJSONString());
 		    out.close();
 		}
 		catch (IOException e)
@@ -73,16 +144,20 @@ public class Client {
 	
 	////////////////////////////////////////////////
 	/// sends the location that the piece is supposed to move 
-	public void sendMovePiece(int x, int y)
+	public void sendMovePiece(int xOrigin, int yOrigin, int xDest, int yDest)
 	{
 		JSONObject obj = new JSONObject();
-		obj.put("xVal", x);
-		obj.put("yVal", y);
+		obj.put("type", "movePiece");
+		obj.put("xOrigin", xOrigin);
+		obj.put("yOrigin", yOrigin);
+		obj.put("xDest", xDest);
+		obj.put("yDest", yDest);
+		
 		
 		try 
 		{
 			OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
-		    out.write(obj.toString());
+		    out.write(obj.toJSONString());
 		    out.close();
 		}
 		catch (IOException e)
