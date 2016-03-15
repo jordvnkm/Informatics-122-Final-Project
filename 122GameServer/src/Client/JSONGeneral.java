41945adc7ea282b785bcad3595b6b3a60cb83f56
msj1294 <msj1294@gmail.com>
2016-03-15 16:01:03 -0700

    JSON clientside General parser
    This class converts JSON commands into ArrayLists<String> the format is
    specified within the file comments.

--- 122GameServer/src/Client/JSONGeneral.java
+++ 122GameServer/src/Client/JSONGeneral.java
@@ -1,22 +1,110 @@
 package Client;
 
-import org.json.simple.JSONObject;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 
-public class JSONGeneral 
+import java.util.ArrayList;
+
+public class JSONGeneral
 {
-	/*******************************************************************************
-	 * 	public static boolean checkGreeting(String s)
-	 * 
-	 * 	Method gets the initial string from the server at connection. The method 
-	 * 		expects the JSON string to be as follows:
-	 * 
-	 *  <Welcome>
-     * 		"Please send the login info"
-	 * 
-	 * 	If this is correct it will return true, otherwise it will return false.
-	 ******************************************************************************/
-	public static boolean checkGreeting(String s)
+	JSONParser parser;
+	String jsonString;
+	Object obj;
+	JSONObject jsonObj;
+	
+	//Receives String in JSONFormat
+	public JSONGeneral(String jsonString){
+		parser = new JSONParser();
+		this.jsonString = jsonString;
+		setupParseObj();
+	}
+	
+	private void setupParseObj(){
+		try{
+			obj = parser.parse(this.jsonString);
+			jsonObj = (JSONObject)obj;
+		}catch(ParseException pe){
+			System.out.println("position: " + pe.getPosition());
+			System.out.println(pe);
+		}
+	}
+	
+	//Returns ArrayList of Strings
+	//For Example:
+	//		If we receive {"type": "MovePiece", "X": 1, "Y": 2}
+	//This returns the String ArrayList:
+	//		["MovePiece", "1", "2"]
+	//Example 2:
+	// 		If we receive {"type": "GameList", "List": ["Othello", "Chutes", "TicTacToe"]}
+	//This returns the String ArrayList:
+	//		["GameList", "Othello", "Chutes", "TicTacToe"]
+	
+	public ArrayList<String> checkType()
 	{
-		return false;
+		String type = jsonObj.get("type").toString();
+		ArrayList<String> returnString = new ArrayList<String>();
+		
+		//Sent to client immediately after connection to verify client is connected to the correct server
+		//Message should be "Please send the login info"
+		if(type.equals("Welcome")){
+			returnString.add("Welcome");
+			returnString.add(jsonObj.get("Msg").toString());
+		}
+		
+		//Sent to client to communicate whether a login was a success or a failure
+		//Message will be either "Successful" or "Failure"
+		else if(type.equals("LoginStatus")){
+			returnString.add("LoginStatus");
+			returnString.add(jsonObj.get("Msg").toString());
+		}
+		
+		//returns a list of all the games
+		else if(type.equals("GameList")){
+			returnString.add("GameList");
+			JSONArray GameList = (JSONArray)jsonObj.get("List");
+			for (int i = 0; i < GameList.size(); i++) {
+				  returnString.add(GameList.get(i).toString());
+			}
+		}
+		//returns a list of players waiting to play
+		else if(type.equals("WaitingToPlay")){
+			returnString.add("WaitingToPlay");
+			JSONArray NameList = (JSONArray)jsonObj.get("List");
+			for (int i = 0; i < NameList.size(); i++) {
+				  returnString.add(NameList.get(i).toString());
+			}
+		}
+		//returns a list of all the players
+		else if(type.equals("PlayerList")){
+			returnString.add("PlayerList");
+			JSONArray NameList = (JSONArray)jsonObj.get("List");
+			for (int i = 0; i < NameList.size(); i++) {
+				  returnString.add(NameList.get(i).toString());
+			}
+		}
+		//For textual messages to display on the clientside GUI
+		else if(type.equals("SendTextMessage")){
+			returnString.add("SendTextMessage");
+			returnString.add(jsonObj.get("message").toString());
+		}
+		
+		//For if the button should be disabled or not
+		else if(type.equals("ButtonDisabled")){
+			returnString.add("ButtonDisabled");
+			returnString.add(jsonObj.get("value").toString());
+		}
+		
+		//Button label
+		else if(type.equals("ButtonText")){
+			returnString.add("ButtonText");
+			returnString.add(jsonObj.get("text").toString());
+		}
+		
+		//When the server passes the gameBoard
+		else if(type.equals("GameBoard")){
+			returnString.add("GameBoard");
+			returnString.add(jsonObj.get("Board").toString());
+		}
+		return returnString;
 	}
 }
