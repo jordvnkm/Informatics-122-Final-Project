7814fd06300fa85389fb143a58b47d047034b2a6
Andy <aschlali@uci.edu>
2016-03-14 17:34:06 -0700

    Board now resizable, but must be done in a special threaded way

--- 122GameServer/src/ClientTesting/GUITesting.java
+++ 122GameServer/src/ClientTesting/GUITesting.java
@@ -1,8 +1,17 @@
 package ClientTesting;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import javafx.application.Platform;
+import Client.Board;
 import Client.MainStage;
 import Client.Tile;
 import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -27,19 +36,44 @@ public class GUITesting extends Application{
 }
 
 class DummyClient implements Runnable{
-	
+	//private final ExecutorService executorService ;
 	private MainStage gui;
-	
+	private int width = 8,height = 8;
 	public DummyClient(MainStage inputgui){
 		gui = inputgui;
-		setupMouseListeners();
-		(new Thread(this)).start();
-	}
+	    gui.getButton().setOnAction((ActionEvent e) -> {
+	    	runtest(20);
+	    });
+	   	(new Thread(this)).start();
+	    }
+		/*executorService = Executors.newSingleThreadExecutor(new ThreadFactory() {
+			@Override
+			public Thread newThread(Runnable r) {
+				Thread thread = new Thread(r);
+				thread.setDaemon(true);
+				return thread;
+			}
+		});
+        gui.getButton().setOnAction((ActionEvent e) -> {
+			final Task<Void> computerMoveTask = new Task<Void>() {
+				@Override
+				public Void call() throws Exception {
+					System.out.println("call made");
+					gui.getBoard().setDimensions(8, 8);
+					System.out.println("setting up listeners");
+					setupMouseListeners(8,8);
+					return null;
+					
+				}
+			};
+			executorService.submit(computerMoveTask);
+        });*/
+	
 
-	public void setupMouseListeners()
+	public void setupMouseListeners(int width, int height)
 	{
-		for(int i=0;i<3;i++)
-        	for(int j=0;j<3;j++){
+		for(int i=0;i<width;i++)
+        	for(int j=0;j<height;j++){
         		gui.getBoard().getTile(i, j).setOnMouseClicked((MouseEvent e) -> {
         			Tile t = (Tile)e.getSource();
                 	int xloc= t.getXlocation();
@@ -49,11 +83,28 @@ class DummyClient implements Runnable{
                 });
         	}
 	}
-	
-	
+	public void runtest(int x){
+		Platform.runLater(new Runnable(){
+
+			@Override
+			public void run() {
+					System.out.println(x+"x"+x);
+					gui.getBoard().setDimensions(x, x);
+					setupMouseListeners(x,x);	
+			}
+		});
+	}
+	//run will not be used in actual client! it is not safe
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		while(true){
+			for(int i=0;i<20;i++){
+				runtest(i);
+				try{Thread.sleep(1000);}catch(InterruptedException e){}
+			}
+			
+		}
 		
 	}
 }
